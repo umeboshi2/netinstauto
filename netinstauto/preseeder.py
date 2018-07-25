170346ba6910e0faeadf20d52ad0f3fc9fb5b580
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-25 18:08:32 -0500

    added preseeder module

---
+++ netinstauto/preseeder.py
@@ -0,0 +1,10 @@
+import pkg_resources
+from jinja2 import Template
+
+
+def get_template():
+    template = 'templates/preseed.cfg.jinja'
+    preseed= pkg_resources.resource_string('netinstauto', template).decode()
+    return Template(preseed)
+
+
