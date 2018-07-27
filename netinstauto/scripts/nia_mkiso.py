8d54ea3a9d936696b5aec21b17ec4fa24cb4e1df
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-26 20:40:46 -0500

    cleanup script

--- netinstauto/scripts/nia_mkiso.py
+++ netinstauto/scripts/nia_mkiso.py
@@ -4,7 +4,6 @@ import subprocess
 import yaml
 
 from ..util import set_writable_recursive
-from ..util import extract_iso, extract_bootblock
 from ..util import create_new_iso
 from ..preseeder import get_template
 
@@ -12,7 +11,6 @@ from ..preseeder import get_template
 def make_preseed_content():
     template = get_template()
     config = yaml.load(open(args.preseed))
-    import pdb ; pdb.set_trace()
     return template.render(**config['preseed_vars'])
 
 
@@ -23,8 +21,8 @@ def create_preseed_file(filename):
 
 
 def main():
+    set_writable_recursive(args.working_dir)
     preseed_filename = os.path.join(args.working_dir, 'preseed.cfg')
-    #if not os.path.isfile(preseed_filename):
     create_preseed_file(preseed_filename)
     create_new_iso('remastered.iso', working_dir=args.working_dir)
     return 0
