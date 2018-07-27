9dbee9214320f7bcc498d6b62aee6d4c607077db
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-07-26 19:33:42 -0500

    cleanup init script

--- netinstauto/scripts/nia_init.py
+++ netinstauto/scripts/nia_init.py
@@ -1,18 +1,13 @@
 import os
 import argparse
 import subprocess
-import pkg_resources
-
-from ..util import set_writable_recursive
-from ..util import extract_iso, extract_bootblock
-
-from ..preseeder import get_template
 
 gitignore = """\
 /netinst
-/*-netinst.iso
+/*.iso
 /isohdpfx.bin
 """
+
 
 def git_init():
     if not os.path.isdir('.git'):
@@ -24,12 +19,11 @@ def git_init():
         subprocess.check_call(cmd)
         cmd = ['git', 'commit', '-m', 'Initial commit.']
         subprocess.check_call(cmd)
-        
+
+
 def main():
     if not os.path.isdir('.git'):
         git_init()
-    
-    # create_new_iso('test.iso')
     return 0
 
 
